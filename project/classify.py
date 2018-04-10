e3f1bb96a26946211a2f39ec0989c9be29367350
Charles Haithcock <cehaith2@ncsu.edu>
2018-04-10 15:51:27 -0400

    added argparse

--- project/classify.py
+++ project/classify.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse 
+import ipdb
+
+
+def main():
+    ipdb.set_trace()
+    args = parse_args()
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data', dest='data_filename', 
+            default='data/iris.data', help='Defaults to iris')
+    parser.add_argument('-n', '--names', dest='name_filename',
+            default='data/iris.names', help='Defaults to iris')
+    parser.add_argument('-k', '--means', dest='k', required=True,
+            help='Centroid count. Must be equal to classes')
+    return parser.parse_args()
 
 
 
@@ -12,15 +30,5 @@ import argparse
 
 
 
-
-
-
-
-
-
-
-
-
-
-if__name__== "__main__":
+if __name__== "__main__":
     main()
