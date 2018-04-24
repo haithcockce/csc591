5765033cde7b20d99cdadf30981d9158891a2e0d
Charles Haithcock <cehaith2@ncsu.edu>
2018-04-24 16:55:30 -0400

    Added kernel code. Now to debug

--- homework3/cudasort.cu
+++ homework3/cudasort.cu
@@ -12,6 +12,52 @@ extern "C"
 
 __global__ void cudasort(float *data, float *temp, int num_of_elements, 
         int subarr_size) {
+
+    unsigned long index;          /* index of thread */
+    unsigned long left_lower;     /* start of left subarray */
+    unsigned long mid;            /* end of left subarray */
+    unsigned long right_upper;    /* end of right subarray */
+    unsigned long right_lower;    /* start orf right subarray (aka mid + 1) */
+    unsigned long temp_index;     /* Index into semi-sorted interim result buffer */
+
+    /* ith work item works on ith and ith + 1 subarrays,
+     * so don't do anything when ith subarray is more than
+     * total subarrays */
+    index = threadIdx.x;
+    left_lower = index * subarr_size;
+    if(left_lower >= num_of_elements) {
+        return;
+    }
+
+    /* Mergesort works on A[p..q] and A[q+1..r], so calculate p == left_lower,
+     * q = mid, q+1 = mid + 1 = right_lower, and r = right_upper
+     */
+    right_upper = left_lower + subarr_size - 1;
+    mid = (left_lower + right_upper) / 2;
+    right_lower = mid + 1;
+
+    temp_index = left_lower;
+    while(left_lower <= mid || right_lower <= right_upper) {
+        if(left_lower > mid) {
+            temp[temp_index++] = data[right_lower++];
+        }
+        else if(right_lower > right_upper) {
+            temp[temp_index++] = data[left_lower++];
+        }
+        else if(data[right_lower] < data[left_lower]) {
+            temp[temp_index++] = data[right_lower++];
+        }
+        else if(data[left_lower] <= data[right_lower]) {
+            temp[temp_index++] = data[left_lower++];
+        }
+    }
+
+    /* Copy the semi-sorted temp content back to the original data set */
+    for(index = threadIdx.x * subarr_size; index <= right_upper; index++) {
+        data[index] = temp[index];
+    }
+
+
 }
 
 int cuda_sort(int num_of_elements, float *data)
@@ -43,7 +89,7 @@ int cuda_sort(int num_of_elements, float *data)
         cudaMemcpy(cuda_temp, temp, size_in_bytes, cudaMemcpyHostToDevice);
 
         /* Execute kernel */
-        cudasort<<<num_of_elements, subarr_size>>>(cuda_data, cuda_temp, 
+        cudasort<<<1, num_of_elements>>>(cuda_data, cuda_temp, 
                 num_of_elements, subarr_size);
 
         /* Read data from GPU (either partially or fully sorted) */
