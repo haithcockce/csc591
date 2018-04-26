fbc06c311fce87a6333f719612d2723f88991523
Ubuntu <ubuntu@ip-172-31-10-199.us-east-2.compute.internal>
2018-04-26 01:36:57 +0000

    It all works!

--- homework3/cudasort.cu
+++ homework3/cudasort.cu
@@ -22,7 +22,6 @@ __global__ void cudasort(float *data, float *temp, int num_of_elements,
     unsigned long mid;            /* end of left subarray */
     unsigned long right_upper;    /* end of right subarray */
     unsigned long right_lower;    /* start orf right subarray (aka mid + 1) */
-    unsigned long temp_index;     /* Index into semi-sorted interim result buffer */
 
     /* Indexing in CUDA is hard. Due to the grid-like nature, getting the i-th
      * thread means calculating the amount of threads/row times the row the i-th
@@ -41,46 +40,50 @@ __global__ void cudasort(float *data, float *temp, int num_of_elements,
 
     /* Mergesort works on A[p..q] and A[q+1..r], so calculate p == left_lower,
      * q = mid, q+1 = mid + 1 = right_lower, and r = right_upper. Last subarray
-     * may end up being shorter than 
-     */
+     * may end up being shorter due to being the remaining amount of the data.*/
     right_upper = (left_lower + subarr_size - 1) >= (num_of_elements - 1) ? 
         (num_of_elements - 1) : (left_lower + subarr_size - 1) ;
     mid = (left_lower + right_upper) / 2;
     right_lower = mid + 1;
+    index = left_lower;
 
-    temp_index = left_lower;
+    /* Begin merging. While we still have elements to merge with... */
     while(left_lower <= mid || right_lower <= right_upper) {
+
+        /* if left subarray is parsed, grab from the right subarray */
         if(left_lower > mid) {
-            temp[temp_index++] = data[right_lower++];
+            temp[index++] = data[right_lower++];
         }
+
+        /* if right subarray is parsed, grab from the left subarray */
         else if(right_lower > right_upper) {
-            temp[temp_index++] = data[left_lower++];
+            temp[index++] = data[left_lower++];
         }
+
+        /* if next smallest element is in right subarray, grab it */
         else if(data[right_lower] < data[left_lower]) {
-            temp[temp_index++] = data[right_lower++];
+            temp[index++] = data[right_lower++];
         }
+
+        /* if next smallest element is in left subarray, grab it */
         else if(data[left_lower] <= data[right_lower]) {
-            temp[temp_index++] = data[left_lower++];
+            temp[index++] = data[left_lower++];
         }
     }
 
-    __syncthreads();
     /* Copy the semi-sorted temp content back to the original data set */
     for(index = global_index * subarr_size; index <= right_upper; index++) {
         data[index] = temp[index];
     }
-
 }
 
 
 int cuda_sort(int num_of_elements, float *data)
 {
-    float *cuda_data;
-    float *cuda_temp;
-
-    float *temp;
-    int subarr_size;
-
+    float *cuda_data;  /* gpu-side storage for data parameter */
+    float *cuda_temp;  /* gpu-side storage for temp work buffer */
+    float *temp;       /* temporary storage that acts like a workspace */
+    int subarr_size;   /* amount of elements in subarrays when merging */
 
     unsigned long size_in_bytes = num_of_elements * sizeof(float);
 
@@ -90,40 +93,33 @@ int cuda_sort(int num_of_elements, float *data)
     cudaMalloc((void **) &cuda_data, size_in_bytes);
     cudaMalloc((void **) &cuda_temp, size_in_bytes);
 
-
-    cudaPrintfInit ();  /* For debugging */
-
-
     /* Merge sort is recursive, but OpenCL doesn't allow recursion (janky)
      * so instead mergesort is then iterative. Each loop iteration is the 
      * next up recursion level starting with the leaf nodes of the recursion
-     * tree. */
-    //int i;
+     * tree. NOTE this implementation requires the data to be a power of 2 and
+     * _will not work_ otherwise. If it does, it's coincidental and I didn't
+     * make it happen :) */
     for(subarr_size = 2; subarr_size <= num_of_elements; 
             subarr_size = subarr_size * 2) {
 
+        /* Final data size is 16777216 so problem space needs to hold this. 256
+         * x 256 x 512 (or more precisely a 256 x 256 grid each with 512 x 1 
+         * blocks) should suffice. */
         dim3 dimGrid(256, 256);
-        //dim3 dimBlock(512, 1);
+
         /* Copy stuff to cuda buffers */
         cudaMemcpy(cuda_data, data, size_in_bytes, cudaMemcpyHostToDevice);
         cudaMemcpy(cuda_temp, temp, size_in_bytes, cudaMemcpyHostToDevice);
 
-        /* Execute kernel */
-        //cudasort<<<1, num_of_elements>>>(cuda_data, cuda_temp, 
-        //        num_of_elements, subarr_size);
-        cudasort<<<dimGrid, 512>>>(cuda_data, 
-                cuda_temp, num_of_elements, subarr_size);
-        cudaThreadSynchronize();
+        /* Execute kernel, 2D grid, (256, 256), with 1D blocks, (512, 1) */
+        cudasort<<<dimGrid, 512>>>(cuda_data, cuda_temp, num_of_elements, 
+                subarr_size);
 
         /* Read data from GPU (either partially or fully sorted) */
         cudaMemcpy(data, cuda_data, size_in_bytes, cudaMemcpyDeviceToHost);
-        cudaThreadSynchronize();
     }
 
     /* Clean up */
-        cudaPrintfDisplay (stdout, true);  /* For debugging */
-    cudaPrintfEnd ();  /* For debugging */ 
-
     free(temp);
     cudaFree(cuda_data);
     cudaFree(cuda_temp);
